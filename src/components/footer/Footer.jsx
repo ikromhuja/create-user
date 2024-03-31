68624b82b1e0e2ce7358d2b6ab3dc2fcde9e0eb6
ikrom marupov <ikrom@gmail.com>
2024-03-31 08:54:44 +0500

    cUser

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+// style
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-container">
+        <p>All rights reserved.</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
